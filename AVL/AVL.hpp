b7f1e7da2ce1fa25e0183b788245effd340e8362
NishchayGithub2004 <162769065+NishchayGithub2004@users.noreply.github.com>
2024-11-16 11:12:57 +0530

    Add files via upload

---
+++ AVL/AVL.hpp
@@ -0,0 +1,61 @@
+#ifndef AVL_HPP
+#define AVL_HPP
+
+#include<iostream>
+#include<algorithm>
+
+class Node
+{
+    public:
+
+    int data;
+    int height;
+    Node* left;
+    Node* right;
+
+    Node(int value)
+    {
+        data = value;
+        height = 1;
+        left = right = nullptr;
+    }
+};
+
+class AVL
+{
+    private: Node* root;
+
+    int height(Node* root) {return root ? root->height : 0;}
+    
+    int getBalance(Node* root) {return root ? height(root->left) - height(root->right) : 0;}
+    
+    Node* rightRotate(Node* y);
+    
+    Node* leftRotate(Node* x);
+    
+    Node* insertNodeHelper(Node* node, int key, bool& isInserted);
+    
+    Node* minValueNode(Node* node);
+
+    Node* deleteNodeHelper(Node* root, int key, bool& nodeDeleted);
+
+    void inOrderHelper(Node* root);
+    
+    void preOrderHelper(Node* root);
+    
+    void postOrderHelper(Node* root);
+
+    public: AVL() {root = nullptr;}
+
+    void insertNode(int value);
+    
+    void deleteNode(int value);
+    
+    void inOrder();
+    
+    void preOrder();
+    
+    void postOrder();
+};
+
+#endif
---
+++ AVL/mainFunction.cpp
@@ -0,0 +1,70 @@
+#include "AVL.hpp"
+#include "left_and_right_rotate.cpp"
+#include "insertNode.cpp"
+#include "deleteNode.cpp"
+#include "traversal.cpp"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    AVL avl;
+    int choice, value;
+
+    while (true)
+    {
+        cout << "\n\nMAIN MENU\n\n";
+        cout << "1. Insert node";
+        cout << "\n2. Delete node";
+        cout << "\n3. Pre order traversal";
+        cout << "\n4. In order traversal";
+        cout << "\n5. Post order traversal";
+        cout << "\n6. Exit";
+        cout << "\n\nEnter your choice : ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+
+            cout << "\n\nEnter value to insert : ";
+            cin >> value;
+            avl.insertNode(value);
+            break;
+
+            case 2:
+
+            cout << "\n\nEnter value to delete : ";
+            cin >> value;
+            avl.deleteNode(value);
+            break;
+
+            case 3:
+
+            cout << "\n\nPre order traversal are : ";
+            avl.preOrder();
+            break;
+
+            case 4:
+
+            cout << "\n\nIn order traversal : ";
+            avl.inOrder();
+            break;
+
+            case 5:
+
+            cout << "\n\nPost order traversal : ";
+            avl.postOrder();
+            break;
+
+            case 6:
+
+            cout << "\n\nExiting the program";
+            return 0;
+
+            default: cout << "\n\nInvalid option entered"; break;
+        }
+    }
+
+    return 0;
+}
